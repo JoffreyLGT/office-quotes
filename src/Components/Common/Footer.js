52309471cd122fa23713aed82d23f16472fdc1aa
Joffrey LAGUT <joff@mbp-de-joffrey.home>
2019-12-17 21:56:02 +0100

    style(Footer): add a margin bottom

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Link } from "@material-ui/core";
 
+const useStyles = makeStyles(theme => ({
+  footerClass: {
+    marginBottom: theme.spacing(2)
+  }
+}));
+
 export default () => {
+  const { footerClass } = useStyles();
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography
+      variant="body2"
+      color="textSecondary"
+      align="center"
+      className={footerClass}
+    >
       {"Copyright © "}
       <Link color="inherit" href="https://github.com/JoffreyLGT">
         Joffrey LAGUT
