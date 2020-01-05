36ba8533d45f428e0c57037d4ceeb9521b979eb6
Joffrey LAGUT <joff@mbp-de-joffrey.home>
2020-01-05 20:27:04 +0100

    refactor(api): remove log from auth

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -13,7 +13,6 @@ const auth = async (req, res, next) => {
     req.token = token;
     next();
   } catch (error) {
-    console.log(error);
     res.status(401).send({ error: "Not authorized to access this resource" });
   }
 };
